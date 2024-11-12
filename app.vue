716f4b7b1f89e7fc38862a2074b13b783cbf4f6c
chriscconte <chris@conte.org>
2024-11-12 15:06:15 -0500

    nuxt deployment

--- app.vue
+++ app.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div>Hi, I'm Chris Conte!</div>
+
+    <div>
+      <h1>Stuff I like</h1>
+      <ul>
+        <li>
+          <a href="https://github.com/feder-cr/Auto_Jobs_Applier_AIHawk">
+            AI Jobs Hawk
+          </a>
+          A hilarious manifestation of the current status of being a developer.
+          Automate your job, from before day one!
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h1>What I'm currently reading</h1>
+      <ul>
+        <li>
+          <b> Lord of the rings </b>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
