1e1d9f75d3ac30c787a34437dfc190ad88cc9754
chriscconte <chris@conte.org>
2024-11-15 19:47:43 -0500

    better mobile support

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="flex">
-    <div class="flex flex-initial w-64">
+  <div class="flex flex-wrap">
+    <div class="flex-initial grow md:w-64">
       <img src="~/assets/img/self.jpg" alt="Picture of Me" />
     </div>
-    <div class="flex grow p-2">
+    <div class="grow p-2">
       <p>
-        Chris is a software engineer based in NYC. 
-        <p> CV
+        Chris is a software engineer based in NYC.
+        <br />
+        CV
         <NuxtLink class="underline" target="_blank" to="/resume.pdf" external>
           here
         </NuxtLink>
-        </p>
       </p>
     </div>
   </div>
